e89818413086c90b8adb9882bc562bf156bb8c6a
Xing Xing <mikespook@gmail.com>
2013-05-17 14:22:39 +0800

    fixed float issue

--- float.go
+++ float.go
@@ -1,7 +1,7 @@
 package py
 
 // #include <Python.h>
-// static inline long floatCheck(PyObject *o) { return PyFloat_Check(o); }
+// static inline double floatCheck(PyObject *o) { return PyFloat_Check(o); }
 import "C"
 import "unsafe"
 
@@ -11,7 +11,7 @@ type Float struct {
 	o C.PyFloatObject
 }
 
-// LongType is the Type object that represents the Long type.
+// FloatType is the Type object that represents the Float type.
 var FloatType = (*Type)(unsafe.Pointer(&C.PyFloat_Type))
 
 func newFloat(obj *C.PyObject) *Float {
@@ -19,7 +19,7 @@ func newFloat(obj *C.PyObject) *Float {
 }
 
 func NewFloat(i float64) *Float {
-	return newFloat(C.PyLong_FromDouble(C.double(i)))
+	return newFloat(C.PyFloat_FromDouble(C.double(i)))
 }
 
 func AsFloat(o *Base) (v *Float, ok bool) {
@@ -29,7 +29,12 @@ func AsFloat(o *Base) (v *Float, ok bool) {
 	return
 }
 
+func NewFloatFromString(s string) *Float {
+        cs := NewString(s)
+	return newFloat(C.PyFloat_FromString((*C.PyObject)(unsafe.Pointer(cs.Obj())), nil))
+}
+
 func (f *Float) Float() float64 {
-	return float64(C.PyLong_AsDouble(f.c()))
+	return float64(C.PyFloat_AsDouble(f.c()))
 }
 
